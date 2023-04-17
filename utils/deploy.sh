128913a89a5714b11d2dd769e7fdef6f31670aae
Dominik Tichy <tichydominik451@gmail.com>
2023-04-17 03:35:33 +0200

    feat: deployment script

--- utils/deploy.sh
+++ utils/deploy.sh
@@ -1,39 +1,72 @@
 #!/bin/bash
 set -x
 
+# This script is used to deploy ACC2 on Metacentrum server.
 # Assumes Ubuntu 22.04
 
-# Upgrade packages to latest versions
-sudo apt-get update
-sudo apt-get -y upgrade
+install_chargefw2() {
+  # Set number of processes for make
+  nproc=$(($(grep -c processor /proc/cpuinfo) + 1))
+
+  cd || exit 1
+  git clone https://github.com/MergunFrimen/ChargeFW2.git ChargeFW2
+  cd ChargeFW2 || exit 1
+  mkdir build && cd build || exit 1
+  cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/home/charge/chargefw2 ..
+  make -j${nproc}
+  sudo make install
+}
+
+install_acc2() {
+  cd || exit 1
+  git clone https://github.com/MergunFrimen/AtomicChargeCalculator2.git ACC2
+  sudo cp -r ACC2/app/* /home/charge/www/ACC2
+}
+
+ACC2_DEPENDENCIES="
+  apache2
+  dos2unix
+  libapache2-mod-wsgi-py3
+  openbabel
+  python3-flask
+  python3-magic
+"
+CHARGEFW2_DEPENDENCIES="
+  cmake
+  g++
+  gemmi-dev
+  git
+  libnanoflann-dev
+  libboost-filesystem-dev
+  libboost-program-options-dev
+  libboost-system-dev
+  libeigen3-dev
+  libfmt-dev
+  nlohmann-json3-dev
+  python3-dev
+  pybind11-dev
+  tao-pegtl-dev
+"
+
+# Install packages needed for building ChargeFW2
+sudo apt-get install -y git g++ cmake libnanoflann-dev gemmi-dev tao-pegtl-dev
+sudo apt-get install -y nlohmann-json3-dev libfmt-dev libeigen3-dev python3-dev
+sudo apt-get install -y libboost-filesystem-dev libboost-system-dev libboost-program-options-dev pybind11-dev
+
+# upgrade packages and install dependencies
+sudo apt update && sudo apt -y upgrade
+sudo apt -y ${ACC2_DEPENDENCIES} ${CHARGEFW2_DEPENDENCIES}
+
+# create directories
+sudo mkdir -p /home/charge/{chargefw2,logs,www/ACC2,.ssh}
 
 # Setup user charge
 sudo useradd charge
-sudo mkdir /home/charge
-
-sudo mkdir /home/charge/.ssh
 sudo chmod 700 /home/charge/.ssh
 sudo cp ~/.ssh/authorized_keys /home/charge/.ssh/authorized_keys
 
-sudo mkdir /home/charge/chargefw2
-sudo mkdir /home/charge/logs
-sudo mkdir -p /home/charge/www/ACC2
-
-# Install necessary packages for ACC2
-ACC2_DEPS="
-apache2
-libapache2-mod-wsgi-py3
-python3-magic
-python3-flask
-"
-# dos2unix
-# openbabel
-sudo apt-get install -y apache2 libapache2-mod-wsgi-py3
-sudo apt-get install -y python3-magic python3-flask dos2unix openbabel
-
 # Configure apache for ACC2 
 sudo sh -c 'sha256sum /etc/passwd > /etc/ACC2.conf'
-
 sudo sh -c 'cat << EOF > /etc/apache2/sites-available/000-ACC2.conf
 <VirtualHost *:80>
         ServerName $(hostname).cerit-sc.cz
@@ -53,32 +86,9 @@ EOF'
 sudo a2dissite 000-default
 sudo a2ensite 000-ACC2
 
-# Install packages needed for building ChargeFW2
-sudo apt-get install -y git g++ cmake libnanoflann-dev gemmi-dev tao-pegtl-dev
-sudo apt-get install -y nlohmann-json3-dev libfmt-dev libeigen3-dev python3-dev
-sudo apt-get install -y libboost-filesystem-dev libboost-system-dev libboost-program-options-dev pybind11-dev
-
-# Set number of processes for make
-nproc=$(($(grep -c processor /proc/cpuinfo) + 1))
-
-# Install latest version of ChargeFW2
-cd || exit 1
-git clone https://github.com/krab1k/ChargeFW2.git ChargeFW2
-cd ChargeFW2 || exit 1
-mkdir build && cd build || exit 1
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/home/charge/chargefw2 ..
-make -j${nproc}
-sudo make install
-
-# Install ACC2
-cd || exit 1
-git clone https://github.com/krab1k/AtomicChargeCalculator2.git ACC2
-sudo cp -r ACC2/app/* /home/charge/www/ACC2
-
-# Install LiteMol component
-sudo mkdir /home/charge/www/ACC2/static/litemol
-wget http://yavanna.ncbr.muni.cz:9877/share/litemol.tar.gz
-sudo tar xvzf litemol.tar.gz -C /home/charge/www/ACC2/static/litemol
+# install external dependencies
+install_chargefw2
+install_acc2
 
 # Setup scripts for easy update
 sudo sh -c 'cat << EOF > /usr/local/bin/update_chargefw2
